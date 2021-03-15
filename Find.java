efd42a5ba9e395f9199815fe60ad7ba649186710
Yago Ananias <yago.crazy@hotmail.com>
2021-03-15 19:54:57 -0300

    Update Find.java

--- Find.java
+++ Find.java
@@ -1,12 +1,13 @@
-public DataItem find(int key) { // find item with key
+public DataItem find(int key) {
+ // find item with key
 // (assumes table not full)
 int hashVal = hashFunc(key); // hash the key
-while(hashArray[hashVal] != null) {// until empty cell,
- // found the key?
-if(hashArray[hashVal].getKey() == key)
-return hashArray[hashVal]; // yes, return item
-++hashVal; // go to next cell
-hashVal %= arraySize; // wrap around if necessary
-}
-return null; // can’t find item
+ while(hashArray[hashVal] != null) {// until empty cell,
+  // found the key?
+ if(hashArray[hashVal].getKey() == key)
+ return hashArray[hashVal]; // yes, return item
+ ++hashVal; // go to next cell
+ hashVal %= arraySize; // wrap around if necessary
+ }
+ return null; // can’t find item
 }
