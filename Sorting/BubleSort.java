f5224becc73cc6f202f1ccc83288fdec909d1cf8
Yago Ananias <yago.crazy@hotmail.com>
2021-03-21 12:38:13 -0300

    Update BubleSort.java

--- Sorting/BubleSort.java
+++ Sorting/BubleSort.java
@@ -1,1 +1,25 @@
+package bublesort;
 
+public class BubleSort {
+  public static void main(String args[]) {
+    int[] v = { 5, 2, 4, 3, 0, 9, 7, 8, 1, 6 };
+    BubleSort bs = new BubleSort();
+    bs.ordenar(v);
+
+    for(int num : v) {
+      System.out.print(num + " ");
+    }
+  }
+  
+  public void ordenar(int[] v) {
+    for(int i = 0; i < v.length - 1; i++) {
+      for(int j = 0; j < v.length - 1 - i; j++) {
+        if(v[j] > v[j + 1]) {
+          int aux = v[j];
+          v[j] = v[j + 1];
+          v[j + 1] = aux;
+        }
+      }
+    }
+  }
+}
