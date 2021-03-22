e0bd29274ac6c10104cec37fdc2ee532dc590549
Yago Ananias <yago.crazy@hotmail.com>
2021-03-22 04:34:02 -0300

    Update Animal.java

--- Sorting/Vectors/Animal.java
+++ Sorting/Vectors/Animal.java
@@ -1,1 +1,26 @@
+public class Animal {
+  private String especie;
+  private String nome;
+  
+  public Animal(String especie, String nome) {
+    this.especie = especie;
+    this.nome = nome;
+  }
+  
+  public String getEspecie() {
+    return especie;  
+  }
+  
+  public void setEspecie(String especie) {
+    this.especie = especie;
+  }
+  
+  public String getNome() {
+    return nome;
+  }
+  
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+}
 
